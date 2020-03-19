7b70418e2067f81e34d2fb08493261bad649e986
Daegyu Lee <37441993+Leedk3@users.noreply.github.com>
2020-03-20 01:42:23 +0900

    Add files via upload

--- src/frenet_c_node.cpp
+++ src/frenet_c_node.cpp
@@ -13,14 +13,20 @@ int main(int argc, char** argv)
     ros::Rate r(50);
 
     //test input
-    std::vector <double> x_vec;
-    std::vector <double> y_vec;
-    Spline spline(x_vec, y_vec);
+    std::vector <double> x_vec{-2.5, 2.0, 4.5, 2.0, 7.5, 3.0, 1.0};
+    std::vector <double> y_vec{0.7, -6, 5, 2.5, 1.0, 5.0, -2.0};
+
+    // Spline spline(x_vec, y_vec);
     Spline2D spline_2d(x_vec, y_vec);
+
+    double result_yaw = spline_2d.calc_yaw(1.0);
+    double result_curv = spline_2d.calc_curvature(1.0);    
+    std::pair<double, double> result_pair = spline_2d.calc_position(1.0);
+    cout << "------------------------ result : ------------------------" << endl;
+    cout << result_yaw << "  " << result_curv <<" " << result_pair.first << " " << result_pair.second << endl;
+    
     while(ros::ok()){
 
-        // path_follower.main_seqeunce();
-        // path_follower.reference_path_fitting();
         ros::spinOnce();
         r.sleep();
     }
