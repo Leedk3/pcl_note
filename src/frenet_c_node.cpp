0a2468fa72910902a457daa384a09dda1b6d5d3d
Daegyu Lee <37441993+Leedk3@users.noreply.github.com>
2020-03-26 16:58:19 +0900

    Add files via upload

--- src/frenet_c_node.cpp
+++ src/frenet_c_node.cpp
@@ -12,8 +12,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "frenet_c"); //initiate node called gps_waypoint
     ros::NodeHandle n;
     ROS_INFO("Initiated frenet_c node");
-    ros::Rate r(50);
-
+    ros::Rate r(5);
     vector<double> wx,wy;
     wx.push_back(0.0);wx.push_back(10.0);wx.push_back(20.5);wx.push_back(30.0);wx.push_back(70.5);
     wy.push_back(0.0);wy.push_back(-6.0);wy.push_back(5.0);wy.push_back(4.5);wy.push_back(0.0);
@@ -25,6 +24,7 @@ int main(int argc, char** argv)
                                {50.0, 2.0} };
 
     vector<double> tx;
+
     vector<double> ty;
     vector<double> tyaw;
     vector<double> tc;
@@ -40,19 +40,31 @@ int main(int argc, char** argv)
 
     double area = 20.0;
 
-    for(int sim = 0; sim < 20; sim++) {
-        auto path = frenet_optimal_planning(csp, s0, c_speed, c_d, c_d_d, c_d_dd, ob);
-
-        s0 = path.s[1];
-        c_d = path.d[1];
-        c_d_d = path.d_d[1];
-        c_d_dd = path.d_dd[1];
-        c_speed = path.s_d[1];
-
-        cout <<"order : " << sim << " " << s0 << " " << c_d << " " << c_d_d << " " << c_d_dd << " " << c_speed << " " << endl;
-    }
+    
 
     while(ros::ok()){
+
+        for(int sim = 0; sim < 20; sim++) {
+            auto path = frenet_optimal_planning(n, csp, s0, c_speed, c_d, c_d_d, c_d_dd, ob);
+            for (int j = 0; j < path.s.size(); j++){
+                cout << "path:" <<path.s.at(j) << endl;
+            }
+
+            if(path.s[0] == -1 && path.d[0] == -1.1){
+                cout << "All path are canceled, no progress" << endl;
+            }
+            else{
+                cout << "Optimal path is found, further progress" << endl;
+                s0 = path.s[1];
+                c_d = path.d[1];
+                c_d_d = path.d_d[1];
+                c_d_dd = path.d_dd[1];
+                c_speed = path.s_d[1];
+            }
+
+            
+            cout <<"order : " << sim << " " << s0 << " " << c_d << " " << c_d_d << " " << c_d_dd << " " << c_speed << " " << endl;
+        }
 
         ros::spinOnce();
         r.sleep();
